75e4eee23380e86018e8a5c8bfbe8f893703f8c9
lazar tiberiu <tiberiulazar2007@gmail.com>
2018-11-27 17:58:20 +0200

    Guess Number

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Tema1  
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random rnd = new Random();
+            int nr = rnd.Next(0, 100);
+            int num;
+           // Console.WriteLine("Numarul este {0}", nr);
+                                   
+            do
+            {
+                Console.WriteLine("Introduceti un nr:\n");
+                num = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ai introdus numarul {0}", num);
+
+                if (num == nr)
+                {
+                    Console.WriteLine("Felicitari ai ghicit numarul, el este {0}", nr);
+                }
+                else
+                {
+                    Console.WriteLine("Mai incearca o data");
+                    if (num > nr)
+                    {
+                        Console.WriteLine("Numarul introdus este mai mare");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Numarul introdus este mai mic");
+                    }
+                }
+            }
+
+            while (num != nr);
+        }
+    }
+}
